37bd7703a2d0b29215edde1092c4fe0cbcaafd79
Roman Sandler <roman.sandler@fiverr.com>
2020-10-05 15:39:34 +0300

    pesemistic todo edit

--- src/components/TodosListItem/TodoEdit.js
+++ src/components/TodosListItem/TodoEdit.js
@@ -1,52 +1,30 @@
-import produce from 'immer'
 import React from 'react'
-import { queryCache, useMutation } from 'react-query'
 import { useTodoItem, useTodos } from '../../hooks'
 import { updateTodo } from '../../services'
 
 function TodoEdit() {
   const editTodoInputRef = React.useRef()
   const { todo, stopEditing } = useTodoItem()
-  const { invalidateAllFilters, currentFilter } = useTodos()
+  const { refetchTodos } = useTodos()
   const [value, setValue] = React.useState(todo.title)
-
-  const [runUpdateTodo] = useMutation(updateTodo, {
-    onMutate: (updatedTodo) => {
-      queryCache.cancelQueries(['todos', currentFilter])
-
-      const previousTodos = queryCache.getQueryData(['todos', currentFilter])
-
-      const updatedTodos = produce(previousTodos, (draftTodos) => {
-        draftTodos.forEach((todo) => {
-          if (todo.id === updatedTodo.id) {
-            todo.title = updatedTodo.title
-          }
-        })
-      })
-
-      queryCache.setQueryData(['todos', currentFilter], updatedTodos)
-
-      stopEditing()
-
-      return () => queryCache.setQueryData(['todos', currentFilter], previousTodos)
-    },
-    onError: (_err, _variables, rollback) => rollback(),
-    onSuccess: () => invalidateAllFilters(),
-  })
 
   const onClickOutsideEdit = React.useCallback(
     async (e) => {
       if (!editTodoInputRef.current) return
       if (!editTodoInputRef.current.contains(e.target)) {
-        runUpdateTodo({ ...todo, title: value })
+        await updateTodo({ ...todo, title: value })
+        await refetchTodos()
+        stopEditing()
       }
     },
-    [runUpdateTodo, todo, value]
+    [refetchTodos, stopEditing, todo, value]
   )
 
-  function onEditSubmit(e) {
+  async function onEditSubmit(e) {
     if (e.key === 'Enter') {
-      runUpdateTodo({ ...todo, title: value })
+      await updateTodo({ ...todo, title: value })
+      await refetchTodos()
+      stopEditing()
     }
   }
 
