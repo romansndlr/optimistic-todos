f981a4168dd5e95302bbc567188a4a8598b6fa8f
Roman Sandler <roman.sandler@fiverr.com>
2020-10-05 11:51:41 +0300

    pessemistic toggle all

--- src/components/TodosToggleAll.js
+++ src/components/TodosToggleAll.js
@@ -1,33 +1,21 @@
 import React from 'react'
-import { queryCache, useMutation } from 'react-query'
 import { useTodos } from '../hooks'
 import { updateTodo } from '../services'
 
 const toggleAll = (todos) => Promise.all(todos.map((todo) => updateTodo(todo)))
 
 function TodosToggleAll() {
-  const { currentFilter, todos, invalidateAllFilters } = useTodos()
+  const { todos, refetchTodos } = useTodos()
   const anyNotCompleted = todos.some((todo) => !todo.completed)
-
-  const [runToggleAll] = useMutation(toggleAll, {
-    onMutate: (updatedTodos) => {
-      queryCache.cancelQueries(['todos', currentFilter])
-
-      const previousTodos = queryCache.getQueryData(['todos', currentFilter])
-
-      queryCache.setQueryData(['todos', currentFilter], updatedTodos)
-
-      return () => queryCache.setQueryData(['todos', currentFilter], previousTodos)
-    },
-    onError: (_err, _variables, rollback) => rollback(),
-    onSuccess: () => invalidateAllFilters(),
-  })
 
   return (
     <React.Fragment>
       <input
         id="toggle-all"
-        onChange={() => runToggleAll(todos.map((todo) => ({ ...todo, completed: anyNotCompleted })))}
+        onChange={async () => {
+          await toggleAll(todos.map((todo) => ({ ...todo, completed: anyNotCompleted })))
+          refetchTodos()
+        }}
         className="toggle-all"
         type="checkbox"
       />
